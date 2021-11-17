7359bc671f2d74a35693476f4cd8436f81ecfa5e
CodeSevenic <sbu.audio.production@gmail.com>
2021-11-17 08:29:17 +0200

    make it better

--- server/config/dev.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://username:1234567890@cluster0.nzaba.mongodb.net/test',
-};
--- server/index.js
+++ server/index.js
@@ -1,20 +1,25 @@
 const express = require('express');
+const env = require('dotenv').config();
 const app = express();
 const cookieParser = require('cookie-parser');
-
-const config = require('./config/key');
-const cors = require('cors');
-// const mongoose = require("mongoose");
-// mongoose
-//   .connect(config.mongoURI, { useNewUrlParser: true })
-//   .then(() => console.log("DB connected"))
-//   .catch(err => console.error(err));
-
 const mongoose = require('mongoose');
-const connect = mongoose
-  .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected...'))
-  .catch((err) => console.log(err));
+
+const cors = require('cors');
+
+const DB = process.env.MONGO_URI;
+
+try {
+  mongoose
+    .connect(DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('Database connected');
+    });
+} catch (error) {
+  console.log(error);
+}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
