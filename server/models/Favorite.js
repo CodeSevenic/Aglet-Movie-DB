7ee83a73e4e609af18e7a92a5ae94850fc84b6e2
CodeSevenic <sbu.audio.production@gmail.com>
2021-11-17 06:19:26 +0200

    make it better

--- server/models/Favorite.js
+++ server/models/Favorite.js
@@ -2,24 +2,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const favoriteSchema = mongoose.Schema({
-    userFrom: {
-        type: Schema.Types.ObjectId,
-        ref:'User'
-    },
-    movieId : {
-        type:String
-    },
-    movieTitle: {
-        type:String
-    },
-    movieImage: {
-        type: String
-    },
-    movieRunTime: {
-        type: String 
-    }
-})
+  userFrom: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  movieId: {
+    type: String,
+  },
+  movieTitle: {
+    type: String,
+  },
+  movieImage: {
+    type: String,
+  },
+  movieRunTime: {
+    type: String,
+  },
+});
 
 const Favorite = mongoose.model('Favorite', favoriteSchema);
 
-module.exports = { Favorite }
+module.exports = Favorite;
--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -1,46 +1,38 @@
 const express = require('express');
 const router = express.Router();
-const { Favorite } = require("../models/Favorite");
+const Favorite = require('../models/Favorite');
 
-const { auth } = require("../middleware/auth");
+const auth = require('../middleware/auth');
 
 //=================================
 //             Favorite
 //=================================
 
-router.post("/favoriteNumber", auth, (req, res) => {
-   
-    //Find Favorite information inside Favorite Collection by Movie ID 
+router.post('/favoriteNumber', auth, (req, res) => {
+  //Find Favorite information inside Favorite Collection by Movie ID
 
-    Favorite.find({"movieId":  req.body.movieId })
-        .exec(( err, favorite ) => {
-            if(err) return res.status(400).send(err)
-            res.status(200).json({ success: true, favoriteNumber: favorite.length })
-        })
-
-
+  Favorite.find({ movieId: req.body.movieId }).exec((err, favorite) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).json({ success: true, favoriteNumber: favorite.length });
+  });
 });
 
+router.post('/favorited', auth, (req, res) => {
+  // Find Favorite Information inside Favorite Collection by Movie Id , userFrom
+  Favorite.find({
+    movieId: req.body.movieId,
+    userFrom: req.body.userFrom,
+  }).exec((err, favorite) => {
+    if (err) return res.status(400).send(err);
 
-router.post("/favorited", auth, (req, res) => {
-   
-   // Find Favorite Information inside Favorite Collection by Movie Id , userFrom 
-   Favorite.find({"movieId":  req.body.movieId , "userFrom": req.body.userFrom })
-    .exec(( err, favorite) => {
-        if(err) return res.status(400).send(err)
+    //How can we know if I already favorite this movie or not ?
+    let result = false;
+    if (favorite.length !== 0) {
+      result = true;
+    }
 
-        //How can we know if I already favorite this movie or not ? 
-        let result = false;
-        if(favorite.length !== 0) {
-            result = true
-        }
-
-        res.status(200).json({ success: true, favorited: result});
-
-    })
-
+    res.status(200).json({ success: true, favorited: result });
+  });
 });
-
-
 
 module.exports = router;
